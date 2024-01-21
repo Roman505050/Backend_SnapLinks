f3fe825d3cfbec22d8aec6d739ea9f0de1b5a419
Roman505050 <myhun59@gmail.com>
2024-01-21 20:37:43 +0200

    +html

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from urllib.request import Request
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from starlette.responses import JSONResponse
+from starlette.staticfiles import StaticFiles
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates("web")
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/ping")
 async def ping():
